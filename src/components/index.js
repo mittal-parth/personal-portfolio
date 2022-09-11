9f7aab017e0eb3301d4a3efa0ad99105a4092c54
Parth <mittalparth22@gmail.com>
2022-09-11 21:36:36 +0530

    add all components to an index file

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+// This file just imports all components and
+// exports them from one place to make code more readable
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Education from "./Education";
+import Skills from "./Skills";
+import Projects from "./Projects";
+import OpenSource from "./OpenSource";
+import ExtraCurricular from "./ExtraCurricular";
+import Footer from "./Footer";
+import Experience from "./Experience";
+
+export {
+    Navbar,
+    Hero,
+    Education,
+    Skills,
+    Projects,
+    OpenSource,
+    ExtraCurricular,
+    Footer,
+    Experience
+};
