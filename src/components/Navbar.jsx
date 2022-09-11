9b55e251b88876d8b3de542ee5c28bfce1fa41a3
Parth <mittalparth22@gmail.com>
2022-09-11 21:35:15 +0530

    add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
+
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <nav className="w-full py-6 flex justify-between items-center navbar">
+      {/* Logo */}
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+
+      {/* List of links */}
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins
+            font-normal
+            cursor-pointer
+            text-[16px]
+            ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}
+            text-white`}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      {/* only for mobile devices, created separately */}
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        {/* shows toggle icon based on its state */}
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          // correct way to change state using the prev
+          // version of the same state using a callback function
+          onClick={() => setToggle((prev) => !prev)}
+        />
+
+        <div
+          className={`${toggle ? "flex" : "hidden"} p-6 bg-black-gradient
+        absolute top-20 right-0 mx-4 my-2
+        min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins
+                font-normal
+                cursor-pointer
+                text-[16px]
+                ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}
+                text-white`}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
