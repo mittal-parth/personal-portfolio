ed1fc81579f3d9dd49008053485aec54f58a45f3
Parth <mittalparth22@gmail.com>
2022-09-12 18:13:19 +0530

    add lottie to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,17 @@
 import styles from "../style";
 import { discount, robot } from "../assets";
 import LetsConnect from "./LetsConnect";
+import Lottie from "react-lottie";
+import animationData from "../lotties/person-coding.json";
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: animationData,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
 
 const Hero = () => {
   return (
@@ -11,13 +22,11 @@ const Hero = () => {
       <div
         className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
       >
-
         {/* Hero text */}
         <div className="flex flex-row justify-between items-center w-full text-white">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[80px] leading-[80px]">
             Hi, there!
-            <br className="sm:block hidden" />{" "}
-            I am
+            <br className="sm:block hidden" /> I am
           </h1>
 
           <div className="ss:flex hidden md:mr-4 mr-0">
@@ -26,19 +35,26 @@ const Hero = () => {
         </div>
 
         <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[80px] leading-[80px] w-full">
-            <span className="text-gradient">Parth Mittal </span>
+          <span className="text-gradient">Parth Mittal </span>
         </h1>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-          Student developer from India who is either busy improving his craft
-          or pondering over the next big idea.
+          Student web-developer from India who is either busy improving his craft or
+          pondering over the next big idea.
         </p>
       </div>
 
-      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-index-[5]"/>
-        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient"></div>
-        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient"></div>
-        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient"></div>
+      <div
+        className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}
+      >
+        <div className="relative z-index-[5] h-[90%] w-[85%]">
+          <Lottie options={defaultOptions} />
+        </div>
+        {/* <img
+          src={robot}
+          alt="billing"
+          className="w-[100%] h-[100%] relative z-index-[5]"
+        /> */}
+        <div className="absolute z-[1] w-[50%] h-[50%] rounded-full bottom-40 white__gradient"></div>
       </div>
 
       <div className={`ss:hidden ${styles.flexCenter}`}>
