eb96911c8c916195275ac6f0c44c46d1e84f6ea7
Vikas Chauhan <vcikas2626@gmail.com>
2024-10-13 16:31:42 +0530

    updated fetching logic

--- src/constants/index.js
+++ src/constants/index.js
@@ -1231,4 +1231,7 @@ export const aboutMe = {
     tagLine: "MTS-1 @Oracle | ex-SDE Intern @Averlon | ex-Tech Lead @IRIS,NITK | ETHIndia'22 Winner | NITK'24",
     intro: "Software Developer from India who is either busy improving his craft or pondering over the next big idea."
 }
-export const itemsToFetch = 20;
+
+export const itemsToFetch = 15;
+
+export const includedRepos = ['publiclab/plots2', 'zulip/zulip'];
