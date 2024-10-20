26c7ee967fd9e25d7adda0a77b54c949c9994bfe
Parth Mittal <mittalparth22@gmail.com>
2024-10-20 23:52:22 +0530

    Move lib dir inside src dir

--- src/lib/helperFunctions.js
+++ src/lib/helperFunctions.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-import { aboutMe, itemsToFetch, includedRepos } from "../src/constants";
+import { aboutMe, itemsToFetch, includedRepos } from "../constants";
 
 export const scrollToSection = (id) => {
   const element = document.getElementById(id);
-  const yOffset = -70; // Adjust this value based on your header height
+  const yOffset = -70;
   const y = element.getBoundingClientRect().top + window.scrollY + yOffset;
 
   window.scrollTo({ top: y, behavior: "smooth" });
