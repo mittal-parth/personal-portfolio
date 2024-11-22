f03f8fc084529ef31701665899287867f435a814
serenebabu2022 <serenebabu22@gmail.com>
2024-11-22 21:02:08 +0000

    added fixed navbar feature with framer-motion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,43 @@
 import { useState } from "react";
 import { close, parthmittal, menu } from "../assets";
+import { motion, useScroll, useMotionValueEvent } from "framer-motion";
 import { navLinks } from "../constants";
 import { scrollToSection } from "../lib/helperFunctions";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  const { scrollY } = useScroll();
+  const [hidden, setHidden] = useState(false);
+  const [prev, setPrev] = useState(0);
+
+
+  // This onupdate is called in scrollY.onChange callback
+  function updateNavbar(latest, prev) {
+    if (latest < prev) {
+      setHidden(false)
+    }
+    else if (latest > 100 && latest > prev) {
+      setHidden(true)
+    }
+  }
+
+  // Using useMotionValueEvent hook to listen to change events
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    updateNavbar(latest, prev)
+    setPrev(latest)
+  })
 
   return (
-    <nav className="w-full  flex justify-between items-center navbar">
+    <motion.nav className={`w-full sm:px-16 px-6 flex justify-between items-center navbar ${hidden ? "opacity-0 -translate-y-full" : "opacity-100 translate-y-0"} bg-primary/50 backdrop-blur shadow-lg fixed z-10`}
+      animate={{
+        y: hidden ? -100 : 0,
+        opacity: hidden ? 0 : 1,
+      }}
+      initial={{ y: 0, opacity: 1 }}
+      transition={{
+        ease: [0.1, 0.25, 0.3, 1],
+        duration: 0.6
+      }}>
       {/* Logo */}
       <a href="#home">
         <img
@@ -69,7 +99,7 @@ const Navbar = () => {
           </ul>
         </div>
       </div>
-    </nav>
+    </motion.nav>
   );
 };
 
