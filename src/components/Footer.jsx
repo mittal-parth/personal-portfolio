2ddbbe31cc4f9fddc9bf3f944cef7e092bd4b535
Pavan L <20628477+Pavan-L09@users.noreply.github.com>
2024-12-23 21:35:48 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -59,7 +59,7 @@ const Footer = () => (
     </div>
     <div className="text-center font-poppins font-normal text-dimWhite text-xs sm:text-sm pb-4">
       <p>
-        © 2024 Pavan Lakku. All rights reserved. Made with 💙 by Parth Mittal & the Open Source Community.
+        © 2024 Pavan Lakku. All rights reserved. Made with 💙 by the Open Source Community.
       </p>
     </div>
   </footer>
