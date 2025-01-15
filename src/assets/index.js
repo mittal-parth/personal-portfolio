f5e77eda2b39031a134b0d76ac8181e2f81b762c
Vikas Chauhan <61468834+A-4-Atom@users.noreply.github.com>
2025-01-15 22:13:30 +0530

    misc: Remove unused imports from assets and constants (#82)

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,34 +1,16 @@
 import parthmittal from "./parth.svg";
 import profilePic from "./profile-pic.jpg";
-import quotes from "./quotes.svg";
-import robot from "./robot.png";
-import send from "./Send.svg";
-import shield from "./Shield.svg";
-import star from "./Star.svg";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import google from "./google.svg";
-import apple from "./apple.svg";
 import arrowUp from "./arrow-up.svg";
-import discount from "./Discount.svg";
-import facebook from "./facebook.svg";
-import instagram from "./instagram.svg";
-import linkedin from "./linkedin.svg";
-import twitter from "./twitter.svg";
 import nitk from "./nitk.png";
 import cluboard from "./cluboard.png";
 import cash_flow from "./cash_flow.jpg";
-import freadom from "./freadom.jpg";
 import ecell from "./ecell.png";
-import bank from "./bank.jpg";
 import gdsc from "./gdsc.png";
 import iris from "./iris.png";
 import genesis from "./genesis.jpg";
-import todo from "./todo.jpg";
-import graphql from "./graphql.png";
 import portfolio from "./portfolio.jpg";
-import zulip from "./zulip.jpg";
-import publiclab from "./publiclab.png";
 import cdc from "./cdc.png";
 import chargeswap from "./chargeswap.png";
 import placeicon from "./place-icon.svg";
@@ -49,35 +31,17 @@ import icon from "./icon.jpg";
 import ethforall from "./ethforall.png";
 
 export {
-  quotes,
-  robot,
-  send,
-  shield,
-  star,
   menu,
   close,
-  google,
-  apple,
   arrowUp,
-  discount,
-  facebook,
-  instagram,
-  linkedin,
-  twitter,
   nitk,
   cluboard,
   cash_flow,
-  freadom,
-  bank,
   gdsc,
   iris,
   ecell,
   genesis,
-  todo,
-  graphql,
   portfolio,
-  publiclab,
-  zulip,
   cdc,
   parthmittal,
   profilePic,
