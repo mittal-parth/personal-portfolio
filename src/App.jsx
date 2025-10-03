5d458628b8cf0900b2dda24e0114cbd9b43433e7
lYNUX <lynux@gmail.com>
2025-10-03 16:26:03 +0530

    Final touchings

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
-// import TestLinkPreview from './components/TestLinkPreview';
+// import TestLinkPreview from './components/TestLinkPreview'; // bro u can remove this it was for debigging
 
 import styles from "./style";
 import {
